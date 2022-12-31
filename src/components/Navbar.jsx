48c011ee8bb42293c70b49833336a998b566d295
Tiago Costa <tiagodpv@gmail.com>
2022-12-31 01:07:48 -0500

    add global styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,13 @@ import { BsPerson } from 'react-icons/bs';
 import { BiSearch } from 'react-icons/bi';
 import { AiOutlineCloset } from 'react-icons/ai';
 import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+import {
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+  FaPinterest,
+  FaInstagram,
+} from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -17,14 +24,20 @@ const Navbar = () => {
         <li>View</li>
         <li>Book</li>
       </ul>
+
       <div className='hidden  md:flex'>
         <BiSearch size={20} />
         <BsPerson size={20} />
       </div>
+
+      {/* Hamburger */}
+
       <div className='md:hidden'>
         <HiOutlineMenuAlt4 size={20} />
       </div>
-      <div>
+
+      {/*Mobile Menu - dropdown*/}
+      <div className='absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col '>
         <ul>
           <h1>BEACHES.</h1>
           <li>Home</li>
@@ -32,6 +45,17 @@ const Navbar = () => {
           <li>Travel</li>
           <li>View</li>
           <li>Book</li>
+          <div>
+            <button>Search</button>
+            <button>Account</button>
+          </div>
+          <div>
+            <FaFacebook />
+            <FaTwitter />
+            <FaYoutube />
+            <FaPinterest />
+            <FaInstagram />
+          </div>
         </ul>
       </div>
     </div>
