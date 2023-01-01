392f214c9dbbdfcd41c6aa888060e59c72ea89b2
Tiago Costa <tiagodpv@gmail.com>
2023-01-01 00:37:56 -0500

    add caroussel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Caroussel from './components/Caroussel';
 import Destinations from './components/Destinations';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
+import Selects from './components/Selects';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Hero />
       <Destinations />
       <Search />
+      <Selects />
+      <Caroussel />
     </div>
   );
 }
